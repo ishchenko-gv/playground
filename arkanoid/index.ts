a32669dd9d67dafd41c28bcc859fc91d7cebab56
Georgii Ishchenko <ishchenko.gv@gmail.com>
2023-06-21 14:04:00 +0100

    arkanoid dev

--- arkanoid/index.ts
+++ arkanoid/index.ts
@@ -7,25 +7,69 @@ import levels from "./levels";
 import { LevelMap } from "./types";
 import Player from "./player";
 
-const canvas = document.getElementById("arkanoid") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d")!;
-const { width, height } = canvas;
+class UI extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    this.canvas = document.createElement("canvas");
+    this.canvas.setAttribute("width", "500");
+    this.canvas.setAttribute("height", "500");
+
+    shadow.appendChild(this.canvas);
+
+    const game = new Game(this.canvas);
+
+    this.canvas.addEventListener("mousemove", (e) => {
+      game.scene.movePaddle(e.movementX);
+    });
+
+    this.canvas.addEventListener("click", () => {
+      this.canvas.requestPointerLock();
+
+      game.scene.launchBall();
+    });
+
+    game.run();
+  }
+
+  canvas: HTMLCanvasElement;
+}
 
 class Game {
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+
+    this.scene = this.createScene(
+      levels[this.level] || levels[0],
+      this.handleBrickDestroy.bind(this),
+      this.handleLevelFinish.bind(this)
+    );
+  }
+
+  canvas: HTMLCanvasElement;
+
+  run() {
+    this.scene.clear();
+    this.scene.update();
+    this.scene.draw();
+
+    requestAnimationFrame(this.run.bind(this));
+  }
+
   player = new Player();
   level = 0;
 
-  scene = this.createScene(
-    levels[this.level] || levels[0],
-    this.handleBrickDestroy.bind(this),
-    this.handleLevelFinish.bind(this)
-  );
+  scene: Scene;
 
   createScene(
     level: LevelMap,
     onBrickDestroy: (score: number) => void,
     onFinish: () => void
   ) {
+    const ctx = this.canvas.getContext("2d")!;
+    const { width, height } = this.canvas;
     const canvasUtil = new CanvasUtil(ctx, width, height);
     const paddle = new Paddle(width);
     const ball = new Ball();
@@ -58,24 +102,4 @@ class Game {
   }
 }
 
-const game = new Game();
-
-canvas.addEventListener("mousemove", (e) => {
-  game.scene.movePaddle(e.movementX);
-});
-
-canvas.addEventListener("click", () => {
-  canvas.requestPointerLock();
-
-  game.scene.launchBall();
-});
-
-function draw() {
-  game.scene.clear();
-  game.scene.update();
-  game.scene.draw();
-
-  requestAnimationFrame(draw);
-}
-
-draw();
+customElements.define("arkanoid-game", UI);
